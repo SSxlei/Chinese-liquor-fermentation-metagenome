c12c7e3bcabc1c727ba401060b6fc2fefaf127e7
Lei Xu <118705094+SSxlei@users.noreply.github.com>
2023-11-10 17:31:51 +0800

    Delete antismash400.sh

--- antismash400.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -c 16
-concat () {
-/public/home/wuq8022600160/anaconda3/envs/antismash7/bin/antismash 11.GTDB_tk/rename/ACT/${1}.fa --taxon bacteria --output-dir 13.antismash/${1} --genefinding-tool prodigal --cb-knownclusters -c 4 --cc-mibig --cb-general --cb-subclusters --fullhmmer
-}
-export -f concat
-cat binid400|parallel -j 4 concat {}
